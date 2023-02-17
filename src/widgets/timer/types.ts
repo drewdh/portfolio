620ed7dd25d5015aaa146b26f4d6e7d96a16160d
Drew Hanberry <drew.hanberry1@gmail.com>
2023-02-17 13:32:47 -0800

    chore: update timer file structure

--- src/widgets/timer/types.ts
+++ src/widgets/timer/types.ts
@@ -1,6 +1,5 @@
 import { NonCancelableCustomEvent } from '@cloudscape-design/components';
 import { SegmentedControlProps } from '@cloudscape-design/components/segmented-control';
-import { ButtonDropdownProps } from '@cloudscape-design/components/button-dropdown';
 
 export enum TimerType {
   Pomodoro = 'pomodoro',
@@ -12,16 +11,7 @@ export enum RunStatus {
   Paused,
   Stopped,
 }
-export enum ActionItem {
-  ResetPomodoroCount = 'reset',
-  Complete = 'complete',
-  Restart = 'restart',
-}
 export interface SetTimerOptions {
   disableAutoStart?: boolean;
 }
-export enum DismissAction {
-
-}
 export type SegmentedControlChangeEvent = NonCancelableCustomEvent<SegmentedControlProps.ChangeDetail>;
-export type ButtonDropdownItemClickEvent = NonCancelableCustomEvent<ButtonDropdownProps.ItemClickDetails>;
