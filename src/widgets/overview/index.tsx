ba349d40f50bf2d45ff2ec21e4c4fbbed049ed6a
Drew Hanberry <drew.hanberry1@gmail.com>
2023-07-19 12:58:59 -0700

    fix(overview): don't default to open nav on mobile

--- src/widgets/overview/index.tsx
+++ src/widgets/overview/index.tsx
@@ -1,33 +1,31 @@
-import { useCallback, useEffect, useState } from 'react';
-import AppLayout, { AppLayoutProps } from '@cloudscape-design/components/app-layout';
+import { useEffect } from 'react';
+import AppLayout from '@cloudscape-design/components/app-layout';
 
 import Breadcrumbs from '../../common/Breadcrumbs';
 import DhSideNavigation from '../../common/side-navigation';
 import Overview from './Overview';
 import useLocalStorage, { LocalStorageKey } from '../../useLocalStorage';
-import { NonCancelableCustomEvent } from '@cloudscape-design/components';
 import { topNavSelector } from '../../top-navigation/constants';
+
+let initialIsNewVisitor: boolean | undefined;
 
 export default function OverviewPage() {
   const [isNewVisitor, setIsNewVisitor] = useLocalStorage<boolean>(LocalStorageKey.IsNewVisitor, true);
-  const [navigationOpen, setNavigationOpen] = useState<boolean>(!isNewVisitor);
-
-  const handleNavigationChange = useCallback((event: NonCancelableCustomEvent<AppLayoutProps.ChangeDetail>): void => {
-    setNavigationOpen(event.detail.open);
-  }, []);
 
   useEffect(() => {
+    if (initialIsNewVisitor === undefined) {
+      initialIsNewVisitor = isNewVisitor;
+    }
     return function() {
       setIsNewVisitor(false);
     }
-  }, [setIsNewVisitor]);
+  }, [isNewVisitor, setIsNewVisitor]);
 
   return (
     <AppLayout
       breadcrumbs={<Breadcrumbs items={[]} />}
       content={<Overview />}
-      onNavigationChange={handleNavigationChange}
-      navigationOpen={navigationOpen}
+      contentType={initialIsNewVisitor ? 'wizard' : 'default'}
       navigation={<DhSideNavigation />}
       toolsHide
       headerSelector={topNavSelector}
