6672d0a0d2cde4d548d8b92c0c6d6b69f9e4c975
Drew Hanberry <drew.hanberry1@gmail.com>
2024-02-13 23:28:33 -0800

    fix: allow submitting feedback by pressing enter

--- src/feedback/Feedback.tsx
+++ src/feedback/Feedback.tsx
@@ -8,9 +8,9 @@ import FormField from '@cloudscape-design/components/form-field';
 import Select from '@cloudscape-design/components/select';
 import Textarea from '@cloudscape-design/components/textarea';
 import RadioGroup from '@cloudscape-design/components/radio-group';
+import Input from '@cloudscape-design/components/input';
 
 import useFeedback from './useFeedback';
-import Input from '@cloudscape-design/components/input';
 
 export default function Feedback({ isVisible, onDismiss }: Props) {
   const {
@@ -51,6 +51,7 @@ export default function Feedback({ isVisible, onDismiss }: Props) {
               <Button
                 loading={isSubmitting}
                 variant="primary"
+                form="feedback"
                 onClick={handleSubmitClick}
               >
                 Submit
@@ -63,64 +64,57 @@ export default function Feedback({ isVisible, onDismiss }: Props) {
       onDismiss={handleDismiss}
       visible={isVisible}
     >
-      {isSuccess && (
-        <Alert type="success">Successfully submitted feedback.</Alert>
-      )}
-      {!isSuccess && (
-        <SpaceBetween size="l">
-          <span>Thank you for taking time to provide feedback.</span>
-          <FormField label="Feedback type">
-            <Select
-              selectedOption={values.type}
-              onChange={handleTypeChange}
-              options={typeOptions}
-            />
-          </FormField>
-          <FormField
-            label="Message"
-            constraintText={messageConstraintText}
-            errorText={errors.message}
-          >
-            <Textarea
-              ref={messageRef}
-              value={values.message}
-              onChange={handleMessageChange}
-            />
-          </FormField>
-          <FormField
-            label="Are you satisfied with your experience?"
-            errorText={errors.satisfied}
-          >
-            <RadioGroup
-              items={satisfiedItems}
-              onChange={handleSatisfiedChange}
-              value={values.satisfied}
-              ref={satisfiedRef}
-            />
-          </FormField>
-          <FormField
-            errorText={errors.email}
-            label={
-              <span>
-                Email - <i>optional</i>
-              </span>
-            }
-            description="If you would like to be contacted about your feedback, enter your email address."
-          >
-            <Input
-              ref={emailRef}
-              placeholder="person.doe@email.com"
-              value={values.email}
-              onChange={handleEmailChange}
-            />
-          </FormField>
-          {isApiError && (
-            <Alert ref={alertRef} type="error">
-              Failed to submit feedback. Try again later.
-            </Alert>
-          )}
-        </SpaceBetween>
-      )}
+      <form id="feedback" onSubmit={(e) => e.preventDefault()}>
+        {isSuccess && <Alert type="success">Successfully submitted feedback.</Alert>}
+        {!isSuccess && (
+          <SpaceBetween size="l">
+            <span>Thank you for taking time to provide feedback.</span>
+            <FormField label="Feedback type">
+              <Select
+                selectedOption={values.type}
+                onChange={handleTypeChange}
+                options={typeOptions}
+              />
+            </FormField>
+            <FormField
+              label="Message"
+              constraintText={messageConstraintText}
+              errorText={errors.message}
+            >
+              <Textarea ref={messageRef} value={values.message} onChange={handleMessageChange} />
+            </FormField>
+            <FormField label="Are you satisfied with your experience?" errorText={errors.satisfied}>
+              <RadioGroup
+                items={satisfiedItems}
+                onChange={handleSatisfiedChange}
+                value={values.satisfied}
+                ref={satisfiedRef}
+              />
+            </FormField>
+            <FormField
+              errorText={errors.email}
+              label={
+                <span>
+                  Email - <i>optional</i>
+                </span>
+              }
+              description="If you would like to be contacted about your feedback, enter your email address."
+            >
+              <Input
+                ref={emailRef}
+                placeholder="person.doe@email.com"
+                value={values.email}
+                onChange={handleEmailChange}
+              />
+            </FormField>
+            {isApiError && (
+              <Alert ref={alertRef} type="error">
+                Failed to submit feedback. Try again later.
+              </Alert>
+            )}
+          </SpaceBetween>
+        )}
+      </form>
     </Modal>
   );
 }
