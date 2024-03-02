377d8268c5f7146a99b9fdec793c5d1b50b7e843
Drew Hanberry <drew.hanberry1@gmail.com>
2024-03-02 12:10:14 -0800

    chore: remove custom theme

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { RouterProvider } from 'react-router';
 import { I18nProvider, importMessages } from '@cloudscape-design/components/i18n';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Theme, applyTheme } from '@cloudscape-design/components/theming';
 
 import { Pathname } from './routes';
 import './index.scss';
@@ -17,96 +16,17 @@ import Login from './auth/login';
 import AuthPage from './auth/auth-page';
 import PasswordReset from './auth/password-reset';
 
-const theme: Theme = {
-  tokens: {},
-  contexts: {
-    header: {
-      tokens: {
-        colorBackgroundLayoutMain: {
-          light: '#ffffff',
-          dark: '#0f1b2a',
-        },
-        colorTextLinkDefault: {
-          light: '#0972d3',
-          dark: '#539fe5',
-        },
-        colorTextLinkHover: {
-          light: '#033160',
-          dark: '#89bdee',
-        },
-        colorTextHeadingDefault: {
-          light: '#000716',
-          dark: '#d1d5db',
-        },
-        colorTextHeadingSecondary: {
-          light: '#414d5c',
-          dark: '#8d99a8',
-        },
-        colorTextStatusInactive: {
-          light: '#5f6b7a',
-          dark: '#8d99a8',
-        },
-        colorTextBreadcrumbCurrent: {
-          light: '#5f6b7a',
-          dark: '#7d8998',
-        },
-        colorTextBreadcrumbIcon: {
-          light: '#7d8998',
-          dark: '#5f6b7a',
-        },
-        colorBackgroundButtonNormalDefault: {
-          light: '#ffffff',
-          dark: '#0f1b2a',
-        },
-        colorBackgroundButtonNormalActive: {
-          light: '#d3e7f9',
-          dark: '#354150',
-        },
-        colorBackgroundButtonNormalDisabled: {
-          light: '#ffffff',
-          dark: '#0f1b2a',
-        },
-        colorBackgroundButtonNormalHover: {
-          light: '#f2f8fd',
-          dark: '#192534',
-        },
-        colorBorderButtonNormalActive: {
-          light: '#033160',
-          dark: '#89bdee',
-        },
-        colorBorderButtonNormalDefault: {
-          light: '#0972d3',
-          dark: '#539fe5',
-        },
-        colorBorderButtonNormalDisabled: {
-          light: '#9ba7b6',
-          dark: '#5f6b7a',
-        },
-        colorBorderButtonNormalHover: {
-          light: '#033160',
-          dark: '#89bdee',
-        },
-        colorTextButtonNormalDefault: {
-          light: '#0972d3',
-          dark: '#539fe5',
-        },
-        colorTextButtonNormalActive: {
-          light: '#033160',
-          dark: '#89bdee',
-        },
-        colorTextButtonNormalHover: {
-          light: '#033160',
-          dark: '#89bdee',
-        },
-        colorTextInteractiveDisabled: {
-          light: '#9ba7b6',
-          dark: '#5f6b7a',
-        },
-      },
-    },
-  },
-};
-applyTheme({ theme });
+interface GlobalFlags {
+  removeHighContrastHeader?: boolean;
+}
+const symbol = Symbol.for('awsui-global-flags');
+interface FlagsHolder {
+  [symbol]?: GlobalFlags;
+}
+if (!(window as FlagsHolder)[symbol]) {
+  (window as FlagsHolder)[symbol] = {};
+}
+(window as FlagsHolder)[symbol]!.removeHighContrastHeader = true;
 
 const router = createBrowserRouter([
   {
