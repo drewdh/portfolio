2f58b115b64de1c94666578a3b41b46cef26d9ef
Drew Hanberry <drew.hanberry1@gmail.com>
2025-04-09 18:28:04 -0700

    refactor: use formik components for feedback

--- src/feedback/feedback.tsx
+++ src/feedback/feedback.tsx
@@ -4,25 +4,22 @@ import Header from '@cloudscape-design/components/header';
 import SpaceBetween from '@cloudscape-design/components/space-between';
 import Box from '@cloudscape-design/components/box';
 import Button from '@cloudscape-design/components/button';
-import FormField from '@cloudscape-design/components/form-field';
-import Select from '@cloudscape-design/components/select';
-import Textarea from '@cloudscape-design/components/textarea';
-import RadioGroup from '@cloudscape-design/components/radio-group';
-import Input from '@cloudscape-design/components/input';
+import { FormikProvider } from 'formik';
 
 import useFeedback from './use-feedback';
+import FormikFormField from 'common/formik/form-field';
+import FormikSelect from 'common/formik/select';
+import FormikTextarea from 'common/formik/textarea';
+import FormikRadioGroup from 'common/formik/radio-group';
+import FormikInput from 'common/formik/input';
 
 export default function Feedback({ visible, onDismiss }: Props) {
   const {
     alertRef,
     emailRef,
-    errors,
+    formik,
     handleDismiss,
-    handleEmailChange,
-    handleMessageChange,
-    handleSatisfiedChange,
     handleSubmitClick,
-    handleTypeChange,
     isApiError,
     isSubmitting,
     isSuccess,
@@ -31,91 +28,79 @@ export default function Feedback({ visible, onDismiss }: Props) {
     satisfiedItems,
     satisfiedRef,
     typeOptions,
-    values,
   } = useFeedback({ onDismiss });
 
   return (
-    <Modal
-      footer={
-        <Box float="right">
-          {isSuccess && (
-            <Button variant="primary" onClick={handleDismiss}>
-              Close
-            </Button>
-          )}
+    <FormikProvider value={formik}>
+      <Modal
+        footer={
+          <Box float="right">
+            {isSuccess && (
+              <Button variant="primary" onClick={handleDismiss}>
+                Close
+              </Button>
+            )}
+            {!isSuccess && (
+              <SpaceBetween size="xs" direction="horizontal">
+                <Button variant="link" form="feedback" formAction="none" onClick={handleDismiss}>
+                  Cancel
+                </Button>
+                <Button
+                  loading={isSubmitting}
+                  variant="primary"
+                  formAction="submit"
+                  form="feedback"
+                  onClick={handleSubmitClick}
+                >
+                  Submit
+                </Button>
+              </SpaceBetween>
+            )}
+          </Box>
+        }
+        header={<Header>Feedback</Header>}
+        onDismiss={handleDismiss}
+        visible={Boolean(visible)}
+      >
+        <form id="feedback" onSubmit={formik.handleSubmit}>
+          {isSuccess && <Alert type="success">Successfully submitted feedback.</Alert>}
           {!isSuccess && (
-            <SpaceBetween size="xs" direction="horizontal">
-              <Button variant="link" onClick={handleDismiss}>
-                Cancel
-              </Button>
-              <Button
-                loading={isSubmitting}
-                variant="primary"
-                form="feedback"
-                onClick={handleSubmitClick}
+            <SpaceBetween size="l">
+              <span>Thank you for taking time to provide feedback.</span>
+              <FormikFormField label="Feedback type" name="type">
+                <FormikSelect name="type" options={typeOptions} />
+              </FormikFormField>
+              <FormikFormField
+                name="message"
+                label="Message"
+                constraintText={messageConstraintText}
               >
-                Submit
-              </Button>
+                <FormikTextarea name="message" ref={messageRef} />
+              </FormikFormField>
+              <FormikFormField name="satisfied" label="Are you satisfied with your experience?">
+                <FormikRadioGroup name="satisfied" items={satisfiedItems} ref={satisfiedRef} />
+              </FormikFormField>
+              <FormikFormField
+                name="email"
+                label={
+                  <span>
+                    Email - <i>optional</i>
+                  </span>
+                }
+                description="If you would like to be contacted about your feedback, enter your email address."
+              >
+                <FormikInput name="email" ref={emailRef} placeholder="person.doe@email.com" />
+              </FormikFormField>
+              {isApiError && (
+                <Alert ref={alertRef} type="error">
+                  Failed to submit feedback. Try again later.
+                </Alert>
+              )}
             </SpaceBetween>
           )}
-        </Box>
-      }
-      header={<Header>Feedback</Header>}
-      onDismiss={handleDismiss}
-      visible={Boolean(visible)}
-    >
-      <form id="feedback" onSubmit={(e) => e.preventDefault()}>
-        {isSuccess && <Alert type="success">Successfully submitted feedback.</Alert>}
-        {!isSuccess && (
-          <SpaceBetween size="l">
-            <span>Thank you for taking time to provide feedback.</span>
-            <FormField label="Feedback type">
-              <Select
-                selectedOption={values.type}
-                onChange={handleTypeChange}
-                options={typeOptions}
-              />
-            </FormField>
-            <FormField
-              label="Message"
-              constraintText={messageConstraintText}
-              errorText={errors.message}
-            >
-              <Textarea ref={messageRef} value={values.message} onChange={handleMessageChange} />
-            </FormField>
-            <FormField label="Are you satisfied with your experience?" errorText={errors.satisfied}>
-              <RadioGroup
-                items={satisfiedItems}
-                onChange={handleSatisfiedChange}
-                value={values.satisfied}
-                ref={satisfiedRef}
-              />
-            </FormField>
-            <FormField
-              errorText={errors.email}
-              label={
-                <span>
-                  Email - <i>optional</i>
-                </span>
-              }
-              description="If you would like to be contacted about your feedback, enter your email address."
-            >
-              <Input
-                ref={emailRef}
-                placeholder="person.doe@email.com"
-                value={values.email}
-                onChange={handleEmailChange}
-              />
-            </FormField>
-            {isApiError && (
-              <Alert ref={alertRef} type="error">
-                Failed to submit feedback. Try again later.
-              </Alert>
-            )}
-          </SpaceBetween>
-        )}
-      </form>
-    </Modal>
+        </form>
+      </Modal>
+    </FormikProvider>
   );
 }
 
--- src/feedback/use-feedback.ts
+++ src/feedback/use-feedback.ts
@@ -1,11 +1,10 @@
 import { Ref, useRef, useState } from 'react';
-import { NonCancelableCustomEvent } from '@cloudscape-design/components';
 import { TextareaProps } from '@cloudscape-design/components/textarea';
 import { AlertProps } from '@cloudscape-design/components/alert';
 import { RadioGroupProps } from '@cloudscape-design/components/radio-group';
 import { SelectProps } from '@cloudscape-design/components/select';
 import { InputProps } from '@cloudscape-design/components/input';
-import { FormikErrors, useFormik } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 import { sendFeedback } from './feedback-api';
@@ -43,7 +42,6 @@ export default function useFeedback({ onDismiss }: Props): State {
   const messageRef = useRef<TextareaProps.Ref>(null);
   const satisfiedRef = useRef<RadioGroupProps.Ref>(null);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
-  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
   const [isApiError, setIsApiError] = useState<boolean>(false);
   const typeOptions: SelectProps.Option[] = [
     {
@@ -64,32 +62,32 @@ export default function useFeedback({ onDismiss }: Props): State {
     },
   ];
 
-  const { errors, handleSubmit, isSubmitting, resetForm, setFieldValue, validateForm, values } =
-    useFormik<Values>({
-      initialValues: {
-        email: '',
-        message: '',
-        satisfied: null,
-        type: typeOptions[0],
-      },
-      validateOnChange: isSubmitted,
-      validationSchema,
-      onSubmit: async (values) => {
-        setIsApiError(false);
-        try {
-          await sendFeedback({
-            message: values.message,
-            type: values.type.value!,
-            satisfied: values.satisfied!,
-            email: values.email,
-          });
-          setIsSuccess(true);
-        } catch {
-          setIsApiError(true);
-          alertRef.current?.focus();
-        }
-      },
-    });
+  const formik = useFormik<Values>({
+    initialValues: {
+      email: '',
+      message: '',
+      satisfied: null,
+      type: typeOptions[0],
+    },
+    validateOnChange: false,
+    validationSchema,
+    onSubmit: async (values) => {
+      setIsApiError(false);
+      try {
+        await sendFeedback({
+          message: values.message,
+          type: values.type.value!,
+          satisfied: values.satisfied!,
+          email: values.email,
+        });
+        setIsSuccess(true);
+      } catch {
+        setIsApiError(true);
+        alertRef.current?.focus();
+      }
+    },
+  });
+  const { isSubmitting, resetForm, validateForm, values, submitForm } = formik;
 
   const satisfiedItems: RadioGroupProps.RadioButtonDefinition[] = [
     {
@@ -102,30 +100,12 @@ export default function useFeedback({ onDismiss }: Props): State {
     },
   ];
 
-  function handleEmailChange(event: NonCancelableCustomEvent<InputProps.ChangeDetail>) {
-    setFieldValue('email', event.detail.value);
-  }
-
-  function handleTypeChange(event: NonCancelableCustomEvent<SelectProps.ChangeDetail>) {
-    setFieldValue('type', event.detail.selectedOption);
-  }
-
-  function handleMessageChange(event: NonCancelableCustomEvent<TextareaProps.ChangeDetail>) {
-    setFieldValue('message', event.detail.value);
-  }
-
-  function handleSatisfiedChange(event: NonCancelableCustomEvent<RadioGroupProps.ChangeDetail>) {
-    setFieldValue('satisfied', event.detail.value as Satisfied);
-  }
-
   const remainingCharacters = 1000 - values.message.length;
   const characterString = remainingCharacters === 1 ? 'character' : 'characters';
   const messageConstraintText = `${remainingCharacters.toLocaleString()} ${characterString} remaining`;
 
   async function handleSubmitClick() {
-    setIsSubmitted(true);
     const errors = await validateForm();
-    handleSubmit();
     if ('message' in errors) {
       return messageRef.current?.focus();
     }
@@ -135,11 +115,11 @@ export default function useFeedback({ onDismiss }: Props): State {
     if ('email' in errors) {
       return emailRef.current?.focus();
     }
+    await submitForm();
   }
 
   function handleDismiss() {
     onDismiss();
-    setIsSubmitted(false);
     setIsSuccess(false);
     setIsApiError(false);
     resetForm();
@@ -148,13 +128,9 @@ export default function useFeedback({ onDismiss }: Props): State {
   return {
     alertRef,
     emailRef,
-    errors,
+    formik,
     handleDismiss,
-    handleEmailChange,
-    handleMessageChange,
-    handleSatisfiedChange,
     handleSubmitClick,
-    handleTypeChange,
     isApiError,
     isSubmitting,
     isSuccess,
@@ -163,7 +139,6 @@ export default function useFeedback({ onDismiss }: Props): State {
     satisfiedItems,
     satisfiedRef,
     typeOptions,
-    values,
   };
 }
 
@@ -174,13 +149,9 @@ interface Props {
 interface State {
   alertRef: Ref<AlertProps.Ref>;
   emailRef: Ref<InputProps.Ref>;
-  errors: FormikErrors<Values>;
+  formik: ReturnType<() => ReturnType<typeof useFormik<Values>>>;
   handleDismiss: () => void;
-  handleEmailChange: (event: NonCancelableCustomEvent<InputProps.ChangeDetail>) => void;
-  handleMessageChange: (event: NonCancelableCustomEvent<TextareaProps.ChangeDetail>) => void;
-  handleSatisfiedChange: (event: NonCancelableCustomEvent<RadioGroupProps.ChangeDetail>) => void;
   handleSubmitClick: () => void;
-  handleTypeChange: (event: NonCancelableCustomEvent<SelectProps.ChangeDetail>) => void;
   isApiError: boolean;
   isSubmitting: boolean;
   isSuccess: boolean;
@@ -189,5 +160,4 @@ interface State {
   satisfiedItems: RadioGroupProps.RadioButtonDefinition[];
   satisfiedRef: Ref<RadioGroupProps.Ref>;
   typeOptions: SelectProps.Options;
-  values: Values;
 }
